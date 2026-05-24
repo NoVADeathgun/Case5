4a34e78c42e659d61956303da4962bf216f585de
Pink-Sun <Fiki.ramadhan.exe@gmail.com>
2026-05-24 18:08:43 +0700

    refactor: simplify boolean check for public holiday

--- src/exam_system.py
+++ src/exam_system.py
@@ -1,4 +1,4 @@
 def check_exam_status(exam_duration, accommodation_status, disconnect_duration, login_time, is_public_holiday):
-    if is_public_holiday == True:
+    if is_public_holiday:
         return "Canceled"
     return "Active"
